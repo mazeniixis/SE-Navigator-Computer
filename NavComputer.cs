c8a98a22145404916668e4163dff087b7510227d
mazeniixis <43690019+mazeniixis@users.noreply.github.com>
2024-05-08 06:11:46 -0600

    Update NavComputer.cs

--- NavComputer.cs
+++ NavComputer.cs
@@ -1,150 +1,190 @@
- // Navigator Computer class
- public class NavComputer
- {
-     readonly IMyShipController _refBlock;
-     readonly List<IMyGyro> myGyros = new List<IMyGyro>();
-     readonly PID _pitchPID, _yawPID;
+// Navigator Computer class
+public class NavComputer
+{
+    readonly IMyShipController _refBlock;
+    readonly List<IMyGyro> myGyros = new List<IMyGyro>();
+    readonly PID _pitchPID, _yawPID;
 
-     Vector3D _rotationPYR, _forwardVector, _upVector, _rotSpeed;
-     double _slowdownAngle, _updatesPerSecond;
-     public AlignMode AlignMode = AlignMode.None;
-     NavStatus _navStatus = NavStatus.Off;
+    Vector3D _rotationPYR = Vector3D.Zero, _forwardVector = Vector3D.Zero, _upVector = Vector3D.Zero, _rotSpeedPYR = Vector3D.Zero;
+    double _slowdownAngle, _updatesPerSecond;
+    public AlignMode AlignMode = AlignMode.None;
+    NavStatus _navStatus = NavStatus.Off;
 
-     public NavComputer(IMyShipController refBlock, PID pitchPID, PID yawPID, double updatesPerSecond, double slowdownAngle)
-     {
-         _refBlock = refBlock;
-         _pitchPID = pitchPID;
-         _yawPID = yawPID;
-         _updatesPerSecond = updatesPerSecond;
-         _slowdownAngle = slowdownAngle;
-     }
+    // thrusters list
+    readonly List<IMyThrust> _forwardThrusts = new List<IMyThrust>();
+    readonly List<IMyThrust> _backwardThrusts = new List<IMyThrust>();
+    readonly List<IMyThrust> _leftThrusts = new List<IMyThrust>();
+    readonly List<IMyThrust> _rightThrusts = new List<IMyThrust>();
+    readonly List<IMyThrust> _upThrusts = new List<IMyThrust>();
+    readonly List<IMyThrust> _downThrusts = new List<IMyThrust>();
 
-     public void AddGyro(IMyGyro gyro)
-     {
-         if (gyro != null)
-             myGyros.Add(gyro);
-     }
+    Action<string> Debug;
 
-     public void AddGyros(List<IMyGyro> gyros) => myGyros.AddRange(gyros);
+    public NavComputer(IMyShipController refBlock, PID pitchPID, PID yawPID, double updatesPerSecond, double slowdownAngle, Action<string> debug)
+    {
+        _refBlock = refBlock;
+        _pitchPID = pitchPID;
+        _yawPID = yawPID;
+        _updatesPerSecond = updatesPerSecond;
+        _slowdownAngle = slowdownAngle;
+        Debug = debug;
+    }
 
-     public bool AlignToHorizon()
-     {
-         if (Vector3D.IsZero(_upVector))
-             return false;
-         SetForwardVector(_upVector.Cross(_refBlock.WorldMatrix.Left));
-         return true;
-     }
+    public void AddGyro(IMyGyro gyro)
+    {
+        if (gyro != null)
+            myGyros.Add(gyro);
+    }
 
-     public void SetForwardVector(Vector3D targetPos) => _forwardVector = targetPos;
+    public void AddGyros(List<IMyGyro> gyros) => myGyros.AddRange(gyros);
 
-     void GetRotationPYR()
-     {
-         var transposedWm = MatrixD.Transpose(_refBlock.WorldMatrix);
-         
-         var forwardVect = Vector3D.Rotate(VectorMath.SafeNormalize(_forwardVector), transposedWm);
+    public void AddThrust(IMyThrust thrust)
+    {
+        if (thrust == null) return;
+        var refOrient = _refBlock.WorldMatrix.GetOrientation();
+        var thrustForward = thrust.WorldMatrix.GetOrientation().Forward;
 
-         Vector3D axis, leftVector = Vector3D.Zero;
-         double angle;
+        if (refOrient.Forward == thrustForward) _forwardThrusts.Add(thrust);
+        else if (refOrient.Backward == thrustForward) _backwardThrusts.Add(thrust);
+        else if (refOrient.Left == thrustForward) _leftThrusts.Add(thrust);
+        else if (refOrient.Right == thrustForward) _rightThrusts.Add(thrust);
+        else if (refOrient.Up == thrustForward) _upThrusts.Add(thrust);
+        else if (refOrient.Down == thrustForward) _downThrusts.Add(thrust);
+    }
 
-         if (!Vector3D.IsZero(_upVector))
-             leftVector = Vector3D.Cross(Vector3D.Rotate(_upVector, transposedWm), forwardVect);
+    public void TestThruster(ThrustDirection direction, float power)
+    {
+        power = MathHelper.Clamp(power, 0, 100) / 100;
+        List<IMyThrust> tempThrust;
+        if (direction == ThrustDirection.Forward) tempThrust = _forwardThrusts;
+        else if (direction == ThrustDirection.Backward) tempThrust = _backwardThrusts;
+        else if (direction == ThrustDirection.Left) tempThrust = _leftThrusts;
+        else if (direction == ThrustDirection.Right) tempThrust = _rightThrusts;
+        else if (direction == ThrustDirection.Up) tempThrust = _upThrusts;
+        else tempThrust = _downThrusts;
 
-         if (Vector3D.IsZero(_upVector) || Vector3D.IsZero(leftVector))
-         {
-             axis = new Vector3D(-forwardVect.Y, forwardVect.X, 0);
-             angle = Math.Acos(MathHelper.Clamp(-forwardVect.Z, -1, 1));
-         }
-         else
-         {
-             leftVector = VectorMath.SafeNormalize(leftVector);
-             var upVector = Vector3D.Cross(_forwardVector, leftVector);
-             var newMatrix = new MatrixD()
-             {
-                 Forward = forwardVect,
-                 Left = leftVector,
-                 Up = upVector
-             };
+        foreach (var t in tempThrust)
+            t.ThrustOverride = power * t.MaxThrust;
+    }
 
-             axis = new Vector3D(newMatrix.M32 - newMatrix.M23, newMatrix.M13 - newMatrix.M31, newMatrix.M21 - newMatrix.M12);
-             double trace = newMatrix.M11 + newMatrix.M22 + newMatrix.M33;
-             angle = Math.Acos(MathHelper.Clamp((trace - 1) * 5, -1, 1));
+    public bool AlignToHorizon()
+    {
+        if (Vector3D.IsZero(_upVector))
+            return false;
+        SetForwardVector(_upVector.Cross(_refBlock.WorldMatrix.Left));
+        return true;
+    }
 
-         }
+    public void SetForwardVector(Vector3D targetPos) => _forwardVector = targetPos;
 
-         if (Vector3D.IsZero(axis))
-         {
-             angle = forwardVect.Z < 0 ? 0 : Math.PI;
-             _rotationPYR = new Vector3D(0, angle, 0);
-         }
-         else
-             _rotationPYR = VectorMath.SafeNormalize(axis) * angle;
-     }
+    void GetRotationPYR()
+    {
+        var transposedWm = MatrixD.Transpose(_refBlock.WorldMatrix);
+        var forwardVect = Vector3D.Rotate(VectorMath.SafeNormalize(_forwardVector), transposedWm);
 
-     void AngleController()
-     {
-         if (_pitchPID != null)
-             _rotSpeed.X = _pitchPID.Control(_rotationPYR.X);
-         if (_yawPID != null)
-             _rotSpeed.Y = _yawPID.Control(_rotationPYR.Y);
+        Vector3D axis, leftVector = Vector3D.Zero;
+        double angle;
 
-         if (Math.Abs(_rotationPYR.X) < _slowdownAngle)
-             _rotSpeed.X = _updatesPerSecond * .5 * _rotationPYR.X;
-         if (Math.Abs(_rotationPYR.Y) < _slowdownAngle)
-             _rotSpeed.Y = _updatesPerSecond * .5 * _rotationPYR.Y;
-     }
+        if (!Vector3D.IsZero(_upVector))
+            leftVector = Vector3D.Cross(Vector3D.Rotate(_upVector, transposedWm), forwardVect);
 
-     void ApplyGyroOverride(bool bOverride = true)
-     {
-         var worldRotationPYR = Vector3D.TransformNormal(_rotSpeed, _refBlock.WorldMatrix);
+        if (Vector3D.IsZero(_upVector) || Vector3D.IsZero(leftVector))
+        {
+            axis = new Vector3D(-forwardVect.Y, forwardVect.X, 0);
+            angle = Math.Acos(MathHelper.Clamp(-forwardVect.Z, -1.0, 1.0));
+        }
+        else
+        {
+            leftVector = VectorMath.SafeNormalize(leftVector);
+            var upVector = Vector3D.Cross(forwardVect, leftVector);
+            var newMatrix = new MatrixD()
+            {
+                Forward = forwardVect,
+                Left = leftVector,
+                Up = upVector
+            };
 
-         for (int g = 0; g < myGyros.Count; g++)
-         {
-             if (myGyros[g].Closed)
-             {
-                 myGyros.RemoveAt(g);
-                 continue;
-             }
+            axis = new Vector3D(newMatrix.M32 - newMatrix.M23, newMatrix.M13 - newMatrix.M31, newMatrix.M21 - newMatrix.M12);
+            double trace = newMatrix.M11 + newMatrix.M22 + newMatrix.M33;
+            angle = Math.Acos(MathHelper.Clamp((trace - 1) * 0.5, -1, 1));
+        }
 
-             var gyro = myGyros[g];
+        if (Vector3D.IsZero(axis))
+        {
+            angle = forwardVect.Z < 0 ? 0 : Math.PI; // THIS IS THE PROBLEM!
+            _rotationPYR = new Vector3D(0, angle, 0);
+        }
+        else
+            _rotationPYR = VectorMath.SafeNormalize(axis) * angle;
+    }
 
-             if (!bOverride)
-             {
-                 gyro.GyroOverride = false;
-                 continue;
-             }
+    void AngleController()
+    {
+        if (_pitchPID != null) _rotSpeedPYR.X = _pitchPID.Control(_rotationPYR.X);
+        if (_yawPID != null) _rotSpeedPYR.Y = _yawPID.Control(_rotationPYR.Y);
 
-             var gyroPYR = Vector3D.TransformNormal(worldRotationPYR, Matrix.Transpose(gyro.WorldMatrix));
+        if (Math.Abs(_rotationPYR.X) < _slowdownAngle) _rotSpeedPYR.X = _updatesPerSecond * .5 * _rotationPYR.X;
+        if (Math.Abs(_rotationPYR.Y) < _slowdownAngle) _rotSpeedPYR.Y = _updatesPerSecond * .5 * _rotationPYR.Y;
+    }
 
-             gyro.Pitch = (float)gyroPYR.X;
-             gyro.Yaw = (float)gyroPYR.Y;
-             gyro.Roll = (float)gyroPYR.Z;
-             gyro.GyroOverride = true;
-         }
-     }
+    void ApplyGyroOverride(bool bOverride = true)
+    {
+        var worldRotationPYR = Vector3D.TransformNormal(_rotSpeedPYR, _refBlock.WorldMatrix);
 
-     void Update()
-     {
-         if ((AlignMode & AlignMode.Natural) != 0) _upVector = -_refBlock.GetNaturalGravity();
-         else if ((AlignMode & AlignMode.Artificial) != 0) _upVector = -_refBlock.GetArtificialGravity();
-         else if ((AlignMode & AlignMode.Total) != 0) _upVector = -_refBlock.GetTotalGravity();
-         else if ((AlignMode & AlignMode.Target) == 0) _upVector = Vector3D.Zero;
-     }
+        for (int g = 0; g < myGyros.Count; g++)
+        {
+            if (myGyros[g].Closed)
+            {
+                myGyros.RemoveAt(g);
+                continue;
+            }
 
-     public void Control()
-     {
-         Debug("Testing...");
-         Debug($"Gravity Direction: {_upVector}");
-         if (_navStatus == NavStatus.Off)
-             return;
-         Update();
-         GetRotationPYR();
-         AngleController();
-         ApplyGyroOverride();
-     }
+            var gyro = myGyros[g];
 
-     public void SetStatus(NavStatus navStatus)
-     {
-         if (navStatus == NavStatus.Off) ApplyGyroOverride(false);
-         _navStatus = navStatus;
-     }
- }
+            if (!bOverride)
+            {
+                gyro.GyroOverride = false;
+                continue;
+            }
+
+            var gyroPYR = Vector3D.TransformNormal(worldRotationPYR, Matrix.Transpose(gyro.WorldMatrix));
+
+            gyro.Pitch = (float)gyroPYR.X;
+            gyro.Yaw = (float)gyroPYR.Y;
+            gyro.Roll = (float)gyroPYR.Z;
+            gyro.GyroOverride = true;
+        }
+    }
+
+    void Update()
+    {
+        if ((AlignMode & AlignMode.Natural) != 0) _upVector = -_refBlock.GetNaturalGravity();
+        else if ((AlignMode & AlignMode.Artificial) != 0) _upVector = -_refBlock.GetArtificialGravity();
+        else if ((AlignMode & AlignMode.Total) != 0) _upVector = -_refBlock.GetTotalGravity();
+        else if ((AlignMode & AlignMode.Target) == 0) _upVector = Vector3D.Zero;
+    }
+
+    public void Control()
+    {
+        if (_navStatus == NavStatus.Off)
+            return;
+        Update();
+        GetRotationPYR();
+        AngleController();
+        ApplyGyroOverride();
+    }
+
+    public void ReadData()
+    {
+        // this is to read the data that I otherwise cannot
+        Debug($"Forward Vector: {_forwardVector}");
+        Debug($"Rotation PYR: {_rotationPYR}");
+        Debug($"Rotation Speed PYR: {_rotSpeedPYR}");
+    }
+
+    public void SetStatus(NavStatus navStatus)
+    {
+        if (navStatus == NavStatus.Off) ApplyGyroOverride(false);
+        _navStatus = navStatus;
+    }
+}
